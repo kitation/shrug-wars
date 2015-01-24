54bfb0598bed4d29463ccec7b29534ea97e1045a
Claire Gowler <claire@yoomee.com>
2015-01-24 14:40:41 +0000

    Clean up menu choices

--- play.rb
+++ play.rb
@@ -12,6 +12,7 @@ my_ship = all_ships.sample
 
 # Take a look at what we get
 
+puts 'Your ship is'
 puts my_ship
 
 # Get a ship for the computer
@@ -20,9 +21,20 @@ cpu_ship = all_ships.sample
 
 # Show a list of options
 
-while true
-	puts my_ship.keys
+ignored_fields = ['url', 'created', 'updated']
+
+continue = true
+while continue
+	puts 'Type q to quit'
+	puts 'Type show to look at your ship'
+	puts my_ship.keys - ignored_fields
 	item = gets.chomp
+	if item == 'q'
+		continue = false
+		break
+	elsif item == 'show'
+		puts my_ship
+	end
 	my_value = my_ship[item]
 	cpu_value = cpu_ship[item]
 	puts "You have #{my_value}, computer has #{cpu_value}"
@@ -32,4 +44,5 @@ while true
 		puts 'Computer wins'
 	end
 end
+
 
