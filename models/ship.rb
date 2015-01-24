b78ac368126a49ad121c768970de82e97b4d83ae
Claire Gowler <claire@yoomee.com>
2015-01-24 15:31:54 +0000

    Ship class

---
+++ models/ship.rb
@@ -0,0 +1,38 @@
+class Ship
+
+  attr_accessor :name, :model, :starship_class, :manufacturer, :length, :hyperdrive_rating, :MGLT,
+    :cost_in_credits, :crew, :passengers, :max_atmosphering_speed, :cargo_capacity, :consumables,
+    :films, :pilots, :url, :created, :edited
+
+  def initialize(data)
+    @name = data['name']
+    @model = data['name']
+    @starship_class = data['starship_class']
+    @manufacturer = data['man']
+    @length = data['length'].to_i
+    @hyperdrive_rating = data['hyperdrive_rating']
+    @MGLT = data['MGLT']
+    @cost_in_credits = data['cost_in_credits'].to_i
+    @crew = data['crew'].to_i
+    @passengers = data['passengers'].to_i
+    @max_atmosphering_speed = data['max_atmosphering_speed'].to_i
+    @cargo_capacity = data['cargo_capacity'].to_i
+    @consumables = data['consumables'].to_i
+    @films = data['films'].length
+    @pilots = data['pilots'].length
+    @url = data['url']
+    @created = data['created']
+    @edited = data['edited']
+  end
+
+  def options
+    ignored_fields = [:@url, :@created, :@edited]
+    (instance_variables - ignored_fields).map { |o| o.to_s.sub('@', '') }
+  end
+
+  def to_s
+    options.each do |option|
+      puts "#{option}: #{send(option)}"
+    end
+  end
+end
--- play.rb
+++ play.rb
@@ -1,5 +1,6 @@
 require 'swapi'
 require 'json'
+require_relative 'models/ship'
 
 # Let's get a list of starships first
 
@@ -8,7 +9,7 @@ all_ships = JSON.parse(ships_request)['results']
 
 # Pick a random ship
 
-my_ship = all_ships.sample
+my_ship = Ship.new(all_ships.sample)
 
 # Take a look at what we get
 
@@ -17,17 +18,15 @@ puts my_ship
 
 # Get a ship for the computer
 
-cpu_ship = all_ships.sample
+cpu_ship = Ship.new(all_ships.sample)
 
 # Show a list of options
-
-ignored_fields = ['url', 'created', 'updated']
 
 continue = true
 while continue
 	puts 'Type q to quit'
 	puts 'Type show to look at your ship'
-	puts my_ship.keys - ignored_fields
+	puts my_ship.options
 	item = gets.chomp
 	if item == 'q'
 		continue = false
@@ -35,10 +34,10 @@ while continue
 	elsif item == 'show'
 		puts my_ship
 	end
-	my_value = my_ship[item]
-	cpu_value = cpu_ship[item]
+	my_value = my_ship.send(item)
+	cpu_value = cpu_ship.send(item)
 	puts "You have #{my_value}, computer has #{cpu_value}"
-	if my_ship[item] > cpu_ship[item]
+	if my_value > cpu_value
 		puts 'Player wins'
 	else
 		puts 'Computer wins'
