7947870f3536a74d8550d53ac206cd13c517441d
Claire Gowler <claire@yoomee.com>
2015-02-08 16:53:26 +0000

    A shell exercise for people to work on

--- models/ship.rb
+++ models/ship.rb
@@ -5,6 +5,7 @@ class Ship
     :films, :pilots, :url, :created, :edited
 
   def initialize(data)
+    # strings
     @name = data['name']
     @model = data['name']
     @starship_class = data['starship_class']
@@ -12,14 +13,20 @@ class Ship
     @length = data['length'].to_i
     @hyperdrive_rating = data['hyperdrive_rating']
     @MGLT = data['MGLT']
+
+    # integers
     @cost_in_credits = data['cost_in_credits'].to_i
     @crew = data['crew'].to_i
     @passengers = data['passengers'].to_i
     @max_atmosphering_speed = data['max_atmosphering_speed'].to_i
     @cargo_capacity = data['cargo_capacity'].to_i
     @consumables = data['consumables'].to_i
+
+    #lists
     @films = data['films'].length
     @pilots = data['pilots'].length
+
+    #API cruft
     @url = data['url']
     @created = data['created']
     @edited = data['edited']
--- play.rb
+++ play.rb
@@ -1,6 +1,5 @@
 require 'swapi'
 require 'json'
-require_relative 'models/ship'
 
 # Let's get a list of starships first
 
@@ -9,39 +8,45 @@ all_ships = JSON.parse(ships_request)['results']
 
 # Pick a random ship
 
-my_ship = Ship.new(all_ships.sample)
+my_ship = all_ships.sample
 
 # Take a look at what we get
 
 puts 'Your ship is'
 puts my_ship
 
-# Get a ship for the computer
-
-cpu_ship = Ship.new(all_ships.sample)
+# TODO: Get a ship for the computer
 
 # Show a list of options
+
+ignored_fields = ['url', 'created', 'updated']
 
 continue = true
 while continue
 	puts 'Type q to quit'
 	puts 'Type show to look at your ship'
-	puts my_ship.options
+
+	# TODO: print out our options 
+
+	# get the option from the user
 	item = gets.chomp
+
+	# if the user types q quit, or if they type show print the ship
 	if item == 'q'
 		continue = false
 		break
 	elsif item == 'show'
 		puts my_ship
 	end
-	my_value = my_ship.send(item)
-	cpu_value = cpu_ship.send(item)
+
+	# TODO: get the values from the hashes
+
+	# who wins?
 	puts "You have #{my_value}, computer has #{cpu_value}"
-	if my_value > cpu_value
+	if my_ship[item] > cpu_ship[item]
 		puts 'Player wins'
 	else
 		puts 'Computer wins'
 	end
 end
-
 
